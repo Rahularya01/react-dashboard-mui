14f54ca7262f459a5bef8a59c681739f712fe0e5
Rahularya01 <55909955+Rahularya01@users.noreply.github.com>
2023-01-20 19:57:11 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import {ColorModeContext, useMode} from "./theme";
+import {CssBaseline, ThemeProvider} from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
+import {Route, Routes} from "react-router-dom";
+import {Dashboard} from "./scenes/dashboard";
+import {Sidebar} from "react-pro-sidebar";
+
+const App = () => {
+	const [theme, colorMode] = useMode();
+	return (
+		<ColorModeContext.Provider value={colorMode}>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<div className='app'>
+					<Sidebar />
+					<main className='content'>
+						<Topbar />
+						<Routes>
+							<Route
+								path='/'
+								element={<Dashboard />}
+							/>
+						</Routes>
+					</main>
+				</div>
+			</ThemeProvider>
+		</ColorModeContext.Provider>
+	);
+};
+
+export default App;
---
+++ src/scenes/global/Sidebar.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+// import {ProSide}
+
+const Sidebar = () => {
+	return <div>Sidebar</div>;
+};
+
+export default Sidebar;
